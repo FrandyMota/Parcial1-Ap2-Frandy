f5b29054adf245b6cfa7ec06d63201064f32e099
Frandy Francisco Mota Rodriguez <frandy_mota@ucne.edu.do>
2020-06-10 18:37:54 -0700

    Add project files.

---
+++ Parcial1-Ap2-Frandy/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Frandy.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= C:\Parcial1-Ap2-Frandy\Parcial.db");
+        }
+    }
+}
